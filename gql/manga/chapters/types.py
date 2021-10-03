cde1285dfe6d85bfd7b22bcf9155117d919cca60
Doctor <thirvondukr@gmail.com>
2021-10-03 03:27:59 +0300

    Remove dead code

--- gql/manga/chapters/types.py
+++ gql/manga/chapters/types.py
@@ -4,26 +4,12 @@ from typing import List, Optional
 from uuid import UUID
 
 import strawberry
-from sqlalchemy import select, cast, func, Integer
-from sqlalchemy.engine import Result
-from sqlalchemy.sql.functions import count
-from strawberry import Private, LazyType
+from strawberry import LazyType
 from strawberry.types import Info
 
-from db.dependencies import get_session
 from db.models.manga.chapters import MangaChapter
-from gql.pagination.types import PaginationPydantic, PaginationInput
 
 MangaType = LazyType["MangaType", "gql.manga.manga.types"]
-
-
-def resolve_recent_chapters(pagination: PaginationInput) -> "RecentChapters":
-    pagination = pagination.to_pydantic()
-    return RecentChapters(
-        page=pagination.page,
-        page_size=pagination.page_size,
-        pagination=pagination,
-    )
 
 
 @strawberry.type(name="MangaChapter")
@@ -58,32 +44,6 @@ class MangaChapterType:
             published_at=chapter.published_at,
             manga_id=chapter.manga_id,
         )
-
-
-@strawberry.type
-class RecentChapters:
-    pagination: Private[PaginationPydantic]
-
-    page: int
-    page_size: int
-
-    @strawberry.field
-    async def chapters(self) -> List[MangaChapterType]:
-        query = select(MangaChapter).order_by(
-            cast((func.substring(MangaChapter.number, "^[0-9]+")), Integer).desc(),
-            MangaChapter.number.desc(),
-        )
-        query = self.pagination.apply_to_query(query)
-
-        async with get_session() as session:
-            result: Result = await session.execute(query)
-        return result.scalars().all()
-
-    @strawberry.field
-    async def total_count(self) -> int:
-        async with get_session() as session:
-            result: Result = await session.execute(select(count(MangaChapter.id)))
-        return typing.cast(int, result.scalar_one())
 
 
 @strawberry.type
