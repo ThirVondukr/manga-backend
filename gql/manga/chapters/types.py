5fba536bb555142d4c02b40b21bcdb24c920b363
Doctor <thirvondukr@gmail.com>
2021-10-03 03:45:07 +0300

    Remove dead code

--- gql/manga/chapters/types.py
+++ gql/manga/chapters/types.py
@@ -1,6 +1,6 @@
 import datetime
 import typing
-from typing import List, Optional
+from typing import Optional
 from uuid import UUID
 
 import strawberry
@@ -44,29 +44,3 @@ class MangaChapterType:
             published_at=chapter.published_at,
             manga_id=chapter.manga_id,
         )
-
-
-@strawberry.type
-class LatestChaptersEdge:
-    node: MangaChapterType
-    cursor: datetime.datetime
-
-    @classmethod
-    def from_chapter(cls, chapter: MangaChapter) -> "LatestChaptersEdge":
-        return cls(
-            node=MangaChapterType.from_orm(chapter),
-            cursor=chapter.published_at,
-        )
-
-
-@strawberry.type
-class LatestChaptersPageInfo:
-    end_cursor: Optional[datetime.datetime]
-    has_next_page: bool
-
-
-@strawberry.type
-class LatestMangaChapters:
-    edges: List[LatestChaptersEdge]
-    page_info: LatestChaptersPageInfo
-    total_count: int
