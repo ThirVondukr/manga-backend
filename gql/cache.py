209569601c5f26ee4eda039d4b5064bd6e56d6c1
Doctor <thirvondukr@gmail.com>
2021-10-03 03:05:12 +0300

    Add lock to AsyncCache to prevent concurrent execution of awaitable

--- gql/cache.py
+++ gql/cache.py
@@ -1,3 +1,4 @@
+import asyncio
 from typing import Awaitable, Callable
 
 _SENTINEL = object()
@@ -7,10 +8,12 @@ class AsyncCache:
     def __init__(self, awaitable: Callable[[], Awaitable]):
         self._awaitable = awaitable
         self._cached = _SENTINEL
+        self._lock = asyncio.Lock()
 
     async def __call__(self):
-        if self._cached is _SENTINEL:
-            self._cached = await self._awaitable()
+        async with self._lock:
+            if self._cached is _SENTINEL:
+                self._cached = await self._awaitable()
         return self._cached
 
     def __await__(self):
