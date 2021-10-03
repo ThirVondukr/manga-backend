1e61afdcb0a52de303e9d143ead25734c9066f68
Doctor <thirvondukr@gmail.com>
2021-10-03 04:27:55 +0300

    Rename MangaPage.imageUrl to image_url

---
+++ alembic/versions/2021_10_03__04_25_e5dcbbaff769_rename_page_imageulr_to_page_image_url.py
@@ -0,0 +1,23 @@
+"""Rename Page.imageUlr to Page.image_url
+
+Revision ID: e5dcbbaff769
+Revises: 8ac1229f6113
+Create Date: 2021-10-03 04:25:56.006422
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = 'e5dcbbaff769'
+down_revision = '8ac1229f6113'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.alter_column(table_name="manga__pages", column_name="imageUrl", new_column_name="image_url")
+
+
+def downgrade():
+    op.alter_column(table_name="manga__pages", column_name="image_url", new_column_name="imageUrl")
