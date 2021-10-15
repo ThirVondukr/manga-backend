639d473d86f9b9027a6481e4a93b6131dbebaf21
Doctor <thirvondukr@gmail.com>
2021-10-15 11:26:43 +0300

    Call load_dotenv in main file

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+"""
+Main file for development purposes
+"""
 import uvicorn
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
+    load_dotenv(".env")
     uvicorn.run(app="app:create_app", factory=True, reload=True, host="0.0.0.0")
