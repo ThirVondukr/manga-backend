ebe6991a4418d96147f7a508e3e26c31a0b97c87
Doctor <thirvondukr@gmail.com>
2021-10-17 15:13:41 +0300

    Move loaders into separate variable

--- gql/app.py
+++ gql/app.py
@@ -1,4 +1,3 @@
-from functools import partial
 from typing import Union, Optional, Any
 
 import strawberry
@@ -8,13 +7,36 @@ from starlette.websockets import WebSocket
 from strawberry.asgi import GraphQL
 from strawberry.dataloader import DataLoader
 
-import gql
 import gql.users
 from gql import users
 from gql.manga.info.loaders import load_manga_info
+from gql.manga.pages.loaders import load_chapter_pages
 from gql.root import Root, Mutation
 from gql.users.context import create_current_user_context
 from gql.users.loaders import load_is_liked_by_viewer, MangaLoaders
+
+_DATA_LOADERS = {
+    MangaLoaders.user_liked_manga_count: DataLoader(users.loaders.load_user_liked_manga_count),
+    MangaLoaders.manga_is_liked_by_viewer: DataLoader(load_is_liked_by_viewer),
+    "manga_loader": DataLoader(load_fn=gql.manga.manga.loaders.load_manga),
+    "manga_info_loader": DataLoader(load_fn=load_manga_info),
+    "manga_art_loader": DataLoader(
+        load_fn=gql.manga.art.loaders.load_manga_art
+    ),
+    "manga_cover_loader": DataLoader(
+        load_fn=gql.manga.art.loaders.load_manga_cover
+    ),
+    "manga_likes_loader": DataLoader(
+        load_fn=gql.manga.manga.loaders.load_manga_likes
+    ),
+    "manga_artists_loader": DataLoader(
+        load_fn=gql.manga.authors.loaders.load_manga_artists
+    ),
+    "manga_writers_loader": DataLoader(
+        load_fn=gql.manga.authors.loaders.load_manga_writers
+    ),
+    "chapter_pages_loader": DataLoader(load_chapter_pages)
+}
 
 
 class MyGraphQL(GraphQL):
@@ -26,33 +48,7 @@ class MyGraphQL(GraphQL):
         ctx: dict = await super().get_context(request, response)
 
         create_current_user_context(request)
-        ctx.update(
-            {
-                MangaLoaders.user_liked_manga_count: DataLoader(
-                    users.loaders.load_user_liked_manga_count
-                ),
-                MangaLoaders.manga_is_liked_by_viewer: DataLoader(
-                    partial(load_is_liked_by_viewer)
-                ),
-                "manga_loader": DataLoader(load_fn=gql.manga.manga.loaders.load_manga),
-                "manga_info_loader": DataLoader(load_fn=load_manga_info),
-                "manga_art_loader": DataLoader(
-                    load_fn=gql.manga.art.loaders.load_manga_art
-                ),
-                "manga_cover_loader": DataLoader(
-                    load_fn=gql.manga.art.loaders.load_manga_cover
-                ),
-                "manga_likes_loader": DataLoader(
-                    load_fn=gql.manga.manga.loaders.load_manga_likes
-                ),
-                "manga_artists_loader": DataLoader(
-                    load_fn=gql.manga.authors.loaders.load_manga_artists
-                ),
-                "manga_writers_loader": DataLoader(
-                    load_fn=gql.manga.authors.loaders.load_manga_writers
-                ),
-            }
-        )
+        ctx.update(_DATA_LOADERS)
         return ctx
 
 
