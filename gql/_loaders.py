9af53d8ee8c39c2ee264b96bd799a27382ece801
Doctor <thirvondukr@gmail.com>
2021-10-22 00:08:05 +0300

    Move `_create_data_loaders` into app.py because of circular import

--- gql/_loaders.py
+++ gql/_loaders.py
@@ -1,10 +1,5 @@
 import enum
 
-from strawberry.dataloader import DataLoader
-
-from gql import users
-from gql.manga import art, authors, info, manga, pages
-
 
 class Loaders(enum.Enum):
     manga = enum.auto()
@@ -21,16 +16,3 @@ class Loaders(enum.Enum):
     chapter_pages = enum.auto()
 
 
-def _create_data_loaders():
-    return {
-        Loaders.manga: DataLoader(manga.loaders.load_manga),
-        Loaders.manga_infos: DataLoader(info.loaders.manga_infos),
-        Loaders.manga_arts: DataLoader(art.loaders.manga_arts),
-        Loaders.manga_cover: DataLoader(art.loaders.manga_cover),
-        Loaders.manga_likes_count: DataLoader(manga.loaders.manga_likes),
-        Loaders.manga_artists: DataLoader(authors.loaders.manga_artists),
-        Loaders.manga_writers: DataLoader(authors.loaders.manga_writers),
-        Loaders.manga_is_liked_by_viewer: DataLoader(users.loaders.manga_is_liked_by_viewer),
-        Loaders.user_liked_manga_count: DataLoader(users.loaders.user_liked_manga_count),
-        Loaders.chapter_pages: DataLoader(pages.loaders.load_chapter_pages)
-    }
--- gql/app.py
+++ gql/app.py
@@ -5,8 +5,12 @@ from starlette.requests import Request
 from starlette.responses import Response
 from starlette.websockets import WebSocket
 from strawberry.asgi import GraphQL
+from strawberry.dataloader import DataLoader
 
-from gql._loaders import _create_data_loaders
+from gql import users
+from gql._loaders import Loaders
+from gql.manga import manga, info, art, authors, pages
+
 from gql.root import Root, Mutation
 from gql.users.context import create_current_user_context
 
@@ -25,3 +29,18 @@ class MyGraphQL(GraphQL):
 
 
 graphql_app = MyGraphQL(schema=strawberry.Schema(query=Root, mutation=Mutation))
+
+
+def _create_data_loaders():
+    return {
+        Loaders.manga: DataLoader(manga.loaders.load_manga),
+        Loaders.manga_infos: DataLoader(info.loaders.manga_infos),
+        Loaders.manga_arts: DataLoader(art.loaders.manga_arts),
+        Loaders.manga_cover: DataLoader(art.loaders.manga_cover),
+        Loaders.manga_likes_count: DataLoader(manga.loaders.manga_likes),
+        Loaders.manga_artists: DataLoader(authors.loaders.manga_artists),
+        Loaders.manga_writers: DataLoader(authors.loaders.manga_writers),
+        Loaders.manga_is_liked_by_viewer: DataLoader(users.loaders.manga_is_liked_by_viewer),
+        Loaders.user_liked_manga_count: DataLoader(users.loaders.user_liked_manga_count),
+        Loaders.chapter_pages: DataLoader(pages.loaders.load_chapter_pages)
+    }
