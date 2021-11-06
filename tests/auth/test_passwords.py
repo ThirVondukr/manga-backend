a410f4d4ba19014afa761e943252cf4e5105390f
Doctor <thirvondukr@gmail.com>
2021-11-06 12:03:04 +0300

    Mark password tests as slow

--- tests/auth/test_passwords.py
+++ tests/auth/test_passwords.py
@@ -25,6 +25,7 @@ def get_user() -> Callable[..., ContextManager[User]]:
 
 @hypothesis.given(password=text())
 @pytest.mark.asyncio
+@pytest.mark.slow
 async def test_can_verify_hashed_password(auth_service, get_user, password):
     with get_user() as user:
         auth_service.update_user_password(user, password)
@@ -33,6 +34,7 @@ async def test_can_verify_hashed_password(auth_service, get_user, password):
 
 @hypothesis.given(password=text(), another_password=text())
 @pytest.mark.asyncio
+@pytest.mark.slow
 async def test_cant_verify_another_password(auth_service, get_user, password: str, another_password: str):
     hypothesis.assume(password != another_password)
 
