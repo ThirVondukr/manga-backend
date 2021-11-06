fa26503f9ce6528418ddfe6f98f4dc3577eb2a7d
Doctor <thirvondukr@gmail.com>
2021-11-06 08:39:45 +0300

    Move auth logic into AuthService

--- modules/auth/router.py
+++ modules/auth/router.py
@@ -1,27 +1,11 @@
-from datetime import datetime, timedelta
-
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestFormStrict
-from jose import jwt
-from sqlalchemy.exc import NoResultFound
 
-from db.models.users import User
-from modules.auth.services import HashingService
+from modules.auth.services import AuthService
 from modules.users.services import UserService
-from settings import auth
 from . import exceptions, schema
 
 auth_router = APIRouter()
-
-
-def create_token(user: User) -> str:
-    expires_at = datetime.utcnow() + timedelta(minutes=auth.token_lifetime_min)
-    claims = {
-        "sub": str(user.id),
-        "exp": expires_at,
-    }
-    token = jwt.encode(claims, key=auth.secret_key, algorithm=auth.algorithm)
-    return token
 
 
 @auth_router.post(
@@ -31,16 +15,13 @@ def create_token(user: User) -> str:
 async def login(
     form_data: OAuth2PasswordRequestFormStrict = Depends(),
     user_service: UserService = Depends(),
-    hash_service: HashingService = Depends(),
+    auth_service: AuthService = Depends(),
 ) -> schema.TokenSchema:
     user = await user_service.get_by_username(username=form_data.username)
     if user is None:
         raise exceptions.InvalidCredentialsError
 
-    if not hash_service.verify_user_password(user=user, password=form_data.password):
+    if not auth_service.verify_user_password(user=user, password=form_data.password):
         raise exceptions.InvalidCredentialsError
 
-    return schema.TokenSchema(
-        access_token=create_token(user),
-        token_type="bearer",
-    )
+    return auth_service.create_token(user)
--- modules/auth/services.py
+++ modules/auth/services.py
@@ -1,9 +1,14 @@
+from datetime import datetime, timedelta
+
+from jose import jwt
 from passlib.context import CryptContext
 
 from db.models.users import User
+from settings import auth
+from . import schema
 
 
-class HashingService:
+class AuthService:
     def __init__(self):
         self.context = CryptContext(
             schemes=["argon2"],
@@ -17,3 +22,16 @@ class HashingService:
 
     def update_user_password(self, user: User, password: str) -> None:
         user.password_hash = self.context.hash(password)
+
+    def create_token(self, user: User) -> schema.TokenSchema:
+        expires_at = datetime.utcnow() + timedelta(minutes=auth.token_lifetime_min)
+        claims = {
+            "sub": str(user.id),
+            "exp": expires_at,
+        }
+        token = jwt.encode(claims, key=auth.secret_key, algorithm=auth.algorithm)
+
+        return schema.TokenSchema(
+            access_token=token,
+            token_type="bearer"
+        )
